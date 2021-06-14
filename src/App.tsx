0c49095ace6c2d279d3dd9bafcb4eeda6333115e
Jacob Runo <jacobkip254@gmail.com>
2021-06-13 23:03:32 -0700

    initialization stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import Products from './Components/Products';
 function App() {
   return (
     <div className="App">
-      <Products />
+      hello
     </div>
   );
 }
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Typography>Kipkurui</Typography>
+        </div>
+    )
+}
+
+export default Home
